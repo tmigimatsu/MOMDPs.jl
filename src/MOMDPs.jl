9c598cb0a5db28a9001ff7c9b45c4eb96659ee7c
Toki Migimatsu <toki.migimatsu@gmail.com>
2018-03-23 17:07:25 -0700

    Fixing MOMDPs

--- src/MOMDPs.jl
+++ src/MOMDPs.jl
@@ -8,7 +8,9 @@ export
     create_fully_obs_transition,
     create_partially_obs_transition,
     fully_obs_space,
-    part_obs_space
+    part_obs_space,
+    initial_fully_obs_distribution,
+    initial_part_obs_distribution
 
 
 abstract type MOMDP{S,A,O} <: POMDP{S,A,O} end
@@ -18,5 +20,8 @@ create_partially_obs_transition(pomdp::MOMDP) = error("$(typeof(pomdp)) does not
 
 fully_obs_space(pomdp::POMDP) = error("$(typeof(pomdp)) does not implement fully_obs_space")   
 part_obs_space(pomdp::POMDP) = error("$(typeof(pomdp)) does not implement part_obs_space")   
+
+initial_fully_obs_distribution(pomdp::POMDP) = error("$(typeof(pomdp)) does not implement fully_obs_space")   
+initial_part_obs_distribution(pomdp::POMDP) = error("$(typeof(pomdp)) does not implement part_obs_space")   
 
 end # module
